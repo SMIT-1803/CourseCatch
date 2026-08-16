c284739fd0ed442c447cb4afe97c7235cf776b80
Smit Sachin Sanghvi <sss101@sfu.ca>
2026-08-16 11:12:03 -0700

    add OTP sign-in with email verification

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,15 +1,28 @@
-import { createClient } from '../lib/supabase/client'
+import { createClient } from '../lib/supabase/server'
 import SignInForm from '@/components/sign-in-form';
+import {Button} from '@/components/ui/button'
+import {signOut} from './actions'
 
 
 export default async function Home() {
-  
-  
+  const supabase = await createClient()
+  const {data} =  await supabase.auth.getClaims()
+  const claims = data?.claims
+
+  if (!data?.claims){
+    return <>
+      <SignInForm />
+    </>
+  }
+
   return (
     <>
-    <div>
-      <SignInForm/>
-    </div>
+      <div>
+        <p>Signed in as {claims.email}</p>
+        <form action={signOut}>
+          <Button type="submit">Sign out</Button>
+        </form>
+      </div>
     </>
   );
 }
